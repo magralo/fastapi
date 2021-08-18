490e3de4d5cc637ce673aede0da0a3a9dc9b2261
magralo <magralo@gmail.com>
2021-08-18 10:32:28 -0500

    test_noAC

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 
 from main import app
 
+import json 
 client = TestClient(app)
 
 
@@ -9,3 +10,10 @@ def test_readyness():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'hello':'worldhgjnsa'}
+
+def test_noAC():
+    data = {"text":"Hola Mundo"}
+    response = client.post("/analize/",params = data)
+    resp = json.loads(response.json())
+    assert response.status_code == 200
+    assert resp["AssetClass"] == ["None"]
